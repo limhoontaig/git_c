312bb8a7b5c164f93b1778bf2e23ffa567e4e17a
limhoontaig <limhoontaig@gmail.com>
2022-08-28 08:00:44 +0900

    add square table using for statements

--- square.c
+++ square.c
@@ -1,4 +1,5 @@
-/* while문을 활용해 제곱수 표를 출력하라 */
+/* while문을 활용해 제곱수 표를 출력하라 Prints a table of squares using a while statements. */
+/* Prints a table of squares using a for statements. */
 
 #include <stdio.h>
 
@@ -16,6 +17,15 @@ int main(void)
         printf("%10d%10d\n", i, i * i);
         ++i;
     }
+
+    printf("This program prints a table of squares. \n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    for (i=1; i <= n; ++i){
+        printf("%10d%10d\n", i, i * i);
+        
+    }    
 
     return 0;
 }
