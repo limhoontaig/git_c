f80ff4c8fefeab82362ca3cb8d05d96e2319cdf6
limhoontaig <limhoontaig@gmail.com>
2022-08-28 08:22:00 +0900

    add example of for statements

--- square.c
+++ square.c
@@ -1,5 +1,6 @@
 /* while문을 활용해 제곱수 표를 출력하라 Prints a table of squares using a while statements. */
 /* Prints a table of squares using a for statements. */
+/* Prints a table of squares using an odd method. */
 
 #include <stdio.h>
 
@@ -7,6 +8,8 @@ int main(void)
 {
     int i;
     int n;
+    int odd;
+    int square;
 
     printf("본 프로그램은 제곱수 표를 출력합니다.\n");
     printf("본 표의 크기를 결정해주세요:  ");
@@ -24,8 +27,20 @@ int main(void)
 
     for (i=1; i <= n; ++i){
         printf("%10d%10d\n", i, i * i);
-        
-    }    
+
+    }
+    
+    printf("This program prints a table of squares. \n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+
+    i = 1;
+    odd = 3;
+    for (square = 1; i <= n; odd += 2) {
+        printf("%10d%10d\n", i, square);
+        ++i;
+        square += odd;
+    }
 
     return 0;
 }
