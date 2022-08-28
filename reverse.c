029a2eb03f69e313f124dd6fdf14936aca37b32b
limhoontaig <limhoontaig@gmail.com>
2022-08-29 06:01:08 +0900

    reverse series using a variable-length array

--- reverse.c
+++ reverse.c
@@ -1,20 +1,28 @@
 /* Reverse a series of numbers */
+/* Reverse a series of numbers using a variable-length array */
 
 # include <stdio.h>
 
-# define N (3)
+// # define N (3)
 
 int main(void) {
-    int a[N] = {0,} ;
+    // int a[N] = {0,} ;
     int i;
-    printf("Enter %d numbers: ", N);
-    for (i = 0; i < N; ++i) {
-        scanf("%d", &a[i]);
+    int reverse_count;
+
+    printf("How many numbers do you want to reverse?: ");
+    scanf("%d", &reverse_count);
+
+    int numbers[reverse_count]; 
+
+    printf("Enter %d numbers: ", reverse_count);
+    for (i = 0; i < reverse_count; ++i) {
+        scanf("%d", &numbers[i]);
     }
 
     printf("In reverse order: ");
-    for (i = N - 1; i >= 0; --i) {
-        printf(" %d", a[i]);
+    for (i = reverse_count - 1; i >= 0; --i) {
+        printf(" %d", numbers[i]);
     }
     printf("\n");
 
